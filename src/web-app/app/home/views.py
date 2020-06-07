812ea6c8162f6b84dbfa3134ae246cf4e4e3c66b
vBlackOut <vladimir.souchet@cuby-hebergs.com>
2020-06-07 03:45:05 +0200

    Fix cpu show frequency

--- src/web-app/app/home/views.py
+++ src/web-app/app/home/views.py
@@ -3,24 +3,22 @@ from django.http import HttpResponse
 from django.contrib.auth import login
 from app.tools.dpam.backends import PAMBackend
 from core.system.stats_system import *
-
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+import datetime
 # Create your views here.
 class Home():
 
     @classmethod
-
     def loginPage(self, request):
 
         if request.method == "GET":
             if request.user.is_authenticated:
-                stats = System_stats()
-
-                statss = stats.stats_cpu
 
                 return render(
                     request,
                     'app/home/index.html',
-                    {"stats": statss}
+                    {"stats": stats_cpu()}
                 )
             else:
                 return render(
--- src/web-app/core/system/stats_system.py
+++ src/web-app/core/system/stats_system.py
@@ -1,26 +1,34 @@
 import subprocess
 from quantiphy import Quantity
+import psutil
 
-class System_stats():
 
-    def __init__(self):
-        pass
+def stats_cpu():
 
-    def stats_cpu(self):
-        cpu_container = {}
-        list_line = []
-        cpu_frequence =  subprocess.Popen(["cat", "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        number_cpu = subprocess.Popen(["nproc", "--all"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        total_frequence_available = int(number_cpu.communicate()[0]) * (int(cpu_frequence.communicate()[0])*1000)
-        container_percente_used =  subprocess.Popen(["docker", "stats", "-a", "--no-stream", "--format", '"{{.Container}}: {{.CPUPerc}}"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    list_line = []
+    cpu_frequence =  subprocess.Popen(["cat", "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    number_cpu = subprocess.Popen(["nproc", "--all"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    total_frequence_available = int(number_cpu) * (int(cpu_frequence)*1000)
+    container_percente_used =  subprocess.Popen(["docker", "stats", "-a", "--no-stream", "--format", '"{{.Container}}: {{.CPUPerc}}"'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
 
-        for container_line in str(container_percente_used.communicate()[0]).split("\\n"):
-           line_parsed = container_line.replace('"', "").replace("b'", "").replace("'", "").replace("%", "").split(": ")
-           line_parsed.pop(0)
-           if "".join(line_parsed[:1]) != "":
-               list_line.append(float("".join(line_parsed[:1])))
+    for container_line in str(container_percente_used).split("\\n"):
+       line_parsed = container_line.replace('"', "").replace("b'", "").replace("'", "").replace("%", "").split(": ")
+       line_parsed.pop(0)
+       if "".join(line_parsed[:1]) != "":
+           list_line.append(float("".join(line_parsed[:1])))
 
-        return {"all_percent_usage": sum(list_line),
-                "max_cpu_freq": Quantity(int(cpu_frequence.communicate()[0])*1000, 'hz').render(),
-                "total_nb_core": int(number_cpu.communicate()[0]),
-                "total_frequency_available": Quantity(total_frequence_available, 'hz').render()}
+    sum_list_cpus = sum(list_line)*1000/(int(number_cpu)*1000)
+    overload = 0
+
+    if sum_list_cpus > 100.9:
+        overload = sum_list_cpus-100
+
+    if psutil.cpu_percent() >= 98 and sum_list_cpus >= 96:
+        sum_list_cpus = 100
+
+    return {"overload": overload,
+            "all_percent_usage": sum_list_cpus,
+            'all_hz_usage': Quantity((total_frequence_available*sum_list_cpus)/100, "hz").render(),
+            "max_cpu_freq": Quantity(int(cpu_frequence)*1000, 'hz').render(),
+            "total_nb_core": int(number_cpu),
+            "total_frequency_available": Quantity(total_frequence_available, 'hz').render()}
